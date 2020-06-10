3a36c516a74333279ca16622f5c28ffbcf7028ab
Simon Thor <simon11.thor@hotmail.se>
2020-06-10 22:10:41 +0200

    Added files to put code in later

---
+++ image_generator.py
@@ -0,0 +1,3 @@
+# In this file, I will attempt to generate comics solely based on the images from xkcd.com
+# Read this: https://towardsdatascience.com/how-to-download-an-image-using-python-38a75cfa21c
+import tensorflow as tf
---
+++ transcript_generator.py
@@ -0,0 +1,4 @@
+# In this file, I will attempt to generate transcripts solely based on the transcripts from explainxkcd.com
+# Read this:
+# - https://www.google.com/search?client=firefox-b-d&sxsrf=ALeKk01j4eZAwde8qWlqJq9RUV0u0M8xVQ%3A1591698474050&ei=KmTfXsjRAqenrgTZnoDwBQ&q=markov+chain+text+generator&oq=markov+chain+text+ge&gs_lcp=CgZwc3ktYWIQAxgAMgUIABDLATIFCAAQywEyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB46BAgAEEc6BAgjECc6BQgAELEDOgUIABCDAToCCAA6BAgAEEM6BwgAELEDEEM6BAgAEApQvhVYi39g5ocBaABwA3gAgAH4A4gB_yeSAQswLjkuNC40LjIuMZgBAKABAaoBB2d3cy13aXo&sclient=psy-ab
+# - https://filiph.github.io/markov/
